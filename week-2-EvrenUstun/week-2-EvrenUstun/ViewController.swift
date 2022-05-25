1ab5cf28aa2e24f2688a968f03c52b99adcd25de
Evren Ustun <ustunnevren@gmail.com>
2022-05-25 10:29:04 +0300

    The second viewcontroller is created.

---
+++ week-2-EvrenUstun/week-2-EvrenUstun/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  week-2-EvrenUstun
+//
+//  Created by Evren Ustun on 25.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var firstLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
